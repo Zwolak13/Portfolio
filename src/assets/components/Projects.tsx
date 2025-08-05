6935799431d89989dde46b71898b4ff540220004
Dawid Zwolak <dawzwolak@gmail.com>
2025-08-05 23:25:57 +0200

    Checking fixes for responsivnes on mobile v2

--- src/assets/components/Projects.tsx
+++ src/assets/components/Projects.tsx
@@ -6,9 +6,9 @@ import ProjectsSingle from "./ProjectsSingle";
 export default function Projects(){
     
 
-    return <section id="four" className={`w-[100vw] lg:!min-h-[100vh] md:min-h-[150vh] min-h-[200vh] bg-black  relative `}>
-        <h1 className="absolute text-blue-400 text-5xl lg:top-20 lg:left-120 top-10 right-25">Projects</h1>
-        <WindowRoll className="absolute lg:top-60 lg:left-[20%] top-40 " title="Projects.log" footerText="Projects">
+    return <section id="four" className={`w-[100vw] lg:!min-h-[100vh] min-h-[140vh] bg-black  relative lg:block flex flex-col items-center`}>
+        <h1 className="lg:absolute text-blue-400 text-5xl lg:top-20 lg:left-160 top-10 right-100 py-10">Projects</h1>
+        <WindowRoll className="lg:absolute lg:top-60 lg:left-[20%] md:left-[10%] top-40 " title="Projects.log" footerText="Projects">
             {projects.map((item,index) => (
             <ProjectsSingle key={index} name={item.name} description={item.descritpion} github={item.github_link} img={item.img_path} stack={item.stack} itchio={item.itchio_link}/>
         ))}
